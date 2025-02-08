7f254b1bb54e28e6e937058adc406adcdd45f3b8
lohieth-rvrl <lohiethrvrl212@gmail.com>
2025-02-08 22:13:28 +0530

    backend issue solved

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'reports',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware", 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Allow all frontend origins (for development)
+CORS_ALLOW_ALL_ORIGINS = True
+
+# OR, to allow only Vite (React frontend)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Allow frontend to access Django backend
+]
+
+# Allow cross-origin requests with credentials (if needed)
+CORS_ALLOW_CREDENTIALS = True
