e7274517943cc7f8ccaf3109fd1efa0d6e69c33e
lohieth-rvrl <lohiethrvrl212@gmail.com>
2025-02-09 04:35:46 +0530

    ui changes

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import EnvironmentalIssueCreateView
+from .views import z
 
 urlpatterns = [
     path('upload/', EnvironmentalIssueCreateView.as_view(), name='upload_issue'),
