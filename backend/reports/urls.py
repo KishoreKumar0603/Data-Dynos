a1f2af8a02e536094cd208871f5f9b224d320cff
KishoreKumar0603 <kishorekumars5643@gmail.com>
2025-02-09 06:23:09 +0530

    register added

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import z
+from .views import *
 
 urlpatterns = [
     path('upload/', EnvironmentalIssueCreateView.as_view(), name='upload_issue'),
+    path("accounts/register/", register, name="register"), 
+
 ]
